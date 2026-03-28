ccbe9ea11fac9ff8503078732710e550c73a67ad
Bao Tram <bao849079@gmail.com>
2026-03-28 11:03:38 +0700

    add home route

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("API is running 🚀")
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -45,8 +49,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', home),  # thêm dòng này
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
